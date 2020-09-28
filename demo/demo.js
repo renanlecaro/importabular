ce9a61dd880ebe0c87cb244f2fc4357bdb815529
Renan LE CARO <renan.lecaro@gmail.com>
2020-09-29 08:15:24 +1000

    Revert "Revert "Updated samples""
    This reverts commit cee316a35a63eb305324d31ca88c0fc796d6a72f.

--- demo/demo.js
+++ demo/demo.js
@@ -20,3 +20,27 @@ sheet.setData([['Hello','World']])
 sheet.destroy()
 
 `);
+
+Array.prototype.forEach.call(
+  document.getElementsByClassName("readable"),
+  (n) => {
+    const pre = document.createElement("pre");
+    pre.className = "auto";
+    let js = n.innerText
+      .trim()
+      .replace('from "../dist/index.js"', 'from "importabular"')
+      .replace('from "../src/index.js"', 'from "importabular/src/index.js"');
+
+    const lines = js.split("\n");
+    const offset = lines[1].match(/ +/)[0].length;
+    js = lines
+      .map((l, i) =>
+        i ? (l.slice(0, offset).trim() ? l.trim() : l.slice(offset)) : l
+      )
+      .join("\n");
+
+    pre.innerText = js;
+
+    n.parentElement.appendChild(pre);
+  }
+);
